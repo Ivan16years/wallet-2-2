66de851473aada3b9fa54c2096d4dce11381fd9a
Ivan16years <101082438+Ivan16years@users.noreply.github.com>
2023-05-15 18:04:50 +0500

    Add files via upload

---
+++ wallet1.1/js.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('.add__input');
+let purcashe__head = document.querySelector('#purcashe__head');
+let purcashe__row = document.querySelector('#purcashe__row');
+let legend__list = document.querySelectorAll('#legend__list');
+
+btn.addEventListener('click', () => {
+
+})
